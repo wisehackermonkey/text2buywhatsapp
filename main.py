0b429bdf951fde4c41211f75941578e9b54cf2a7
Oran C <oranbusiness@gmail.com>
2022-01-09 14:06:11 -0700

    add: working round trip and 1 level selection

--- main.py
+++ main.py
@@ -21,6 +21,35 @@ db = deta.Base(DATABASE_NAME)
 
 app = Flask(__name__, static_url_path='')
 
+welcome_text ="""Text to buy: 
+#1 - latest posts
+#2 - free stuff
+#3 - for sale
+Type 1, 2, or 3"""
+
+
+
+def send_message(message, from_number, to_number, status_callback, media_url):
+    """send_message
+
+    Args:
+        message ([str]): [description]
+        from_number ([str]): [description]
+        to_number ([str]): [description]
+        status_callback ([str]): [description]
+        media_url ([List]): [description]
+    """
+    message = client.messages.create(
+                            from_=from_number,
+                            to=to_number,
+                            body=message,
+                            status_callback=status_callback,
+                            media_url=media_url,
+                        )
+    return message
+
+formate_input = lambda text: text.strip()
+
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
     """Respond to incoming calls with a simple text message."""
@@ -35,23 +64,60 @@ def sms_reply():
 def sms_recieve():
     number = request.form["From"]
     message_body = request.form["Body"]
+    
+    from_number = 'whatsapp:+14155238886' 
+    status_callback = 'https://ekpert.deta.dev/status'
+    media_url = []
+    option = formate_input(message_body)
+    
+    if option == "1":
+        message = """
+Latest stuff:
+1) $185 AirPod Pro  (Santa Clara)
+2) $30  snow tire chains (gilroy)
+3) $10  Yard chairs (alameda)
+4) $35  Wheelchair (santa cruz)
+5) More...
+"""
+        message = client.messages.create(
+                                from_=from_number,
+                                to=myphonenumber,
+                                body=message,
+                                status_callback=status_callback,
+                                media_url=media_url,
+                            )
+        # send_message(message, from_number, myphonenumber, status_callback, media_url)
+    else:
+        message = """
+Please type "1","2","3"
+"""
+        send_message(message, from_number, myphonenumber, status_callback, media_url)
+
+        
     print(f" Number: {number}, Message Body: {message_body}")
+    
     resp = MessagingResponse()
     resp.message(f"/sms_recieve worked!  Number: {number}, Message Body: {message_body}")
+    
     return (str(resp),204)
 
 
 @app.route("/send", methods=['GET'])
 def send_message():
     """EX: https://ekpert.deta.dev/send"""
-
+    # message = send_message(welcome_text,
+    #     'whatsapp:+14155238886',
+    #     myphonenumber,
+    #     'https://ekpert.deta.dev/status',
+    #     ['https://ekpert.deta.dev/static/car_v1.jpg']
+    # )
+    
     message = client.messages.create(
-                              from_='whatsapp:+14155238886',
-                              body='#1-back,#2-more info: 2015 Honda accord sport low miles - $18,500 (sacramento)',
-                              to=myphonenumber,
-                              status_callback='https://ekpert.deta.dev/status',
-                              media_url=['https://ekpert.deta.dev/static/car_v1.jpg'],
-                          )
+                        from_='whatsapp:+14155238886',
+                        to=myphonenumber,
+                        body=welcome_text,
+                        status_callback='https://ekpert.deta.dev/status',
+                 )
 
     print(message.sid)
     return str(message.sid)
